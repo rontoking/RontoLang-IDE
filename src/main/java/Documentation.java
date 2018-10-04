231a8d90ec5c508559c86a382f707ccb3c591df2
RontoKing <rontoniya@gmail.com>
2018-10-04 19:00:50 +0200

    RontoLang IDE

--- src/main/java/Documentation.java
+++ src/main/java/Documentation.java
@@ -4,10 +4,10 @@ import javafx.scene.control.TreeView;
 
 public class Documentation {
     public static String AUTOCOMPLETE_TEXT;
-    private static StringBuilder stringBuilder;
+    private static StringBuilder strBuilder;
 
     public static TreeView get(){
-        stringBuilder = new StringBuilder();
+        strBuilder = new StringBuilder();
         TreeView treeView = new TreeView();
         treeView.setCellFactory(tv ->  {
             TreeCell<String> cell = new TreeCell<String>() {
@@ -31,7 +31,7 @@ public class Documentation {
         treeView.setRoot(new TreeItem<>("RontoLang Documentation"));
         treeView.getRoot().getChildren().addAll(valueTypes(), globalVariables(), instructions());
         Utility.expand(treeView.getRoot());
-        AUTOCOMPLETE_TEXT = stringBuilder.toString();
+        AUTOCOMPLETE_TEXT = strBuilder.toString();
         return treeView;
     }
 
@@ -84,17 +84,17 @@ public class Documentation {
         add(treeItem, 1, "getLower", true, "str");
         add(treeItem, 1, "list", true, "char[]");
 
-        add(treeItem, 1, "substring(int start)", "str");
-        add(treeItem, 1, "substring(int start, int end)", "str");
+        add(treeItem, 1, "substr(int start)", "str");
+        add(treeItem, 1, "substr(int start, int end)", "str");
         add(treeItem, 1, "charAt(int index)", "char");
-        add(treeItem, 1, "replace(string target, string replacement)", "str");
-        add(treeItem, 1, "split(string splitter)", "string[]");
-        add(treeItem, 1, "split(string splitter, int limit)", "string[]");
-        add(treeItem, 1, "indexOf(string s)", "int");
-        add(treeItem, 1, "lastIndexOf(string s)", "int");
-        add(treeItem, 1, "contains(string s)", "bool");
-        add(treeItem, 1, "startsWith(string s)", "bool");
-        add(treeItem, 1, "endsWith(string s)", "bool");
+        add(treeItem, 1, "replace(str target, str replacement)", "str");
+        add(treeItem, 1, "split(str splitter)", "str[]");
+        add(treeItem, 1, "split(str splitter, int limit)", "str[]");
+        add(treeItem, 1, "indexOf(str s)", "int");
+        add(treeItem, 1, "lastIndexOf(str s)", "int");
+        add(treeItem, 1, "contains(str s)", "bool");
+        add(treeItem, 1, "startsWith(str s)", "bool");
+        add(treeItem, 1, "endsWith(str s)", "bool");
 
         // Complex variables
 
@@ -109,7 +109,7 @@ public class Documentation {
         add(treeItem, 1, "add(any item)", "void");
         add(treeItem, 1, "remove(int index)", "void");
         add(treeItem, 1, "swap(int index1, int index2)", "void");
-        add(treeItem, 1, "join(string joiner)", "str");
+        add(treeItem, 1, "join(str joiner)", "str");
         add(treeItem, 1, "insert(int index, any item)", "void");
         add(treeItem, 1, "startsWith(list start)", "bool");
         add(treeItem, 1, "endsWith(list end)", "bool");
@@ -152,14 +152,14 @@ public class Documentation {
         add(treeItem, 1, "empty", true, "void");
         add(treeItem, 1, "emptyFiles", true, "void");
 
-        add(treeItem, 1, "write(string s)", "void");
-        add(treeItem, 1, "append(string s)", "void");
+        add(treeItem, 1, "write(str s)", "void");
+        add(treeItem, 1, "append(str s)", "void");
         add(treeItem, 1, "writeBytes(byte[] b)", "void");
         add(treeItem, 1, "appendBytes(byte[] b)", "void");
-        add(treeItem, 1, "rename(string s)", "void");
+        add(treeItem, 1, "rename(str s)", "void");
         add(treeItem, 1, "move(file f)", "void");
-        add(treeItem, 1, "child(string name)", "file");
-        add(treeItem, 1, "sibling(string name)", "file");
+        add(treeItem, 1, "child(str name)", "file");
+        add(treeItem, 1, "sibling(str name)", "file");
 
         add(treeItem, 0, "img");
 
@@ -170,8 +170,8 @@ public class Documentation {
 
         add(treeItem, 0, "font");
 
-        add(treeItem, 1, "width(string text)", "float");
-        add(treeItem, 1, "height(string text)", "float");
+        add(treeItem, 1, "width(str text)", "float");
+        add(treeItem, 1, "height(str text)", "float");
 
         add(treeItem, 0, "sound");
 
@@ -284,7 +284,7 @@ public class Documentation {
         add(treeItem, 0, "construct", true, "void");
         add(treeItem, 0, "this", true, "any");
         add(treeItem, 0, "super", true, "any");
-        add(treeItem, 0, "pressedKeys", true, "string[]");
+        add(treeItem, 0, "pressedKeys", true, "str[]");
         add(treeItem, 0, "mouseX", true, "int");
         add(treeItem, 0, "mouseY", true, "int");
         add(treeItem, 0, "mousePos", true, "point");
@@ -304,8 +304,8 @@ public class Documentation {
 
         add(treeItem, 0, "client", true, "client");
 
-        add(treeItem, 1, "join(string ip, int port)", "bool");
-        add(treeItem, 1, "join(string ip, int port, int writeBufferSize, int objectBufferSize)", "bool");
+        add(treeItem, 1, "join(str ip, int port)", "bool");
+        add(treeItem, 1, "join(str ip, int port, int writeBufferSize, int objectBufferSize)", "bool");
         add(treeItem, 1, "send(any value)", "void");
 
         add(treeItem, 0, "socket", true, "socket");
@@ -332,9 +332,9 @@ public class Documentation {
 
         add(treeItem, 0, "prefs", true, "void");
 
-        add(treeItem, 1, "open(string name)", "void");
-        add(treeItem, 1, "put(string key, string value)", "void");
-        add(treeItem, 1, "get(string key)", "void");
+        add(treeItem, 1, "open(str name)", "void");
+        add(treeItem, 1, "put(str key, str value)", "void");
+        add(treeItem, 1, "get(str key)", "void");
 
         add(treeItem, 0, "console", true, "void");
 
@@ -362,6 +362,7 @@ public class Documentation {
         add(treeItem, 0, "if(bool condition){BLOCK}","void");
         add(treeItem, 0, "else if(bool condition){BLOCK}", "void");
         add(treeItem, 0, "else{BLOCK}", "void");
+        add(treeItem, 0, "func(PARAMETERS){BLOCK}","func");
         add(treeItem, 0, "return(any value)", "void");
         add(treeItem, 0, "print(any value)", "void");
         add(treeItem, 0, "println(any value)", "void");
@@ -378,7 +379,7 @@ public class Documentation {
         add(treeItem, 0, "thread(bool isDaemon){BLOCK}", "void");
         add(treeItem, 0, "runLater(bool trueForGDXFalseForGUI){BLOCK}", "void");
         add(treeItem, 0, "wait(int milliseconds)", "void");
-        add(treeItem, 0, "exec{string expression}", "void");
+        add(treeItem, 0, "exec{str expression}", "void");
         add(treeItem, 0, "random()", "float");
         add(treeItem, 0, "random(int range)", "int");
         add(treeItem, 0, "random(int start, int end)", "int");
@@ -401,39 +402,39 @@ public class Documentation {
         add(treeItem, 0, "sqrt(double num)", "double");
         add(treeItem, 0, "toDeg(double radians)", "double");
         add(treeItem, 0, "toRad(double degrees)", "double");
-        add(treeItem, 0, "img(string name)", "img");
+        add(treeItem, 0, "img(str name)", "img");
         add(treeItem, 0, "img(file f)", "img");
-        add(treeItem, 0, "sound(string name)", "sound");
+        add(treeItem, 0, "sound(str name)", "sound");
         add(treeItem, 0, "sound(file f)", "sound");
-        add(treeItem, 0, "music(string name)", "music");
+        add(treeItem, 0, "music(str name)", "music");
         add(treeItem, 0, "music(file f)", "music");
         add(treeItem, 0, "font(int size)", "font");
-        add(treeItem, 0, "font(string name, int size)", "font");
+        add(treeItem, 0, "font(str name, int size)", "font");
         add(treeItem, 0, "font(file f, int size)", "font");
-        add(treeItem, 0, "string(any value)", "str");
-        add(treeItem, 0, "int(string s)", "int");
-        add(treeItem, 0, "float(string s)", "float");
-        add(treeItem, 0, "double(string s)", "double");
-        add(treeItem, 0, "internal(string path)", "file");
-        add(treeItem, 0, "local(string path)", "file");
-        add(treeItem, 0, "external(string path)", "file");
-        add(treeItem, 0, "absolute(string path)", "file");
-        add(treeItem, 0, "classpath(string path)", "file");
+        add(treeItem, 0, "str(any value)", "str");
+        add(treeItem, 0, "int(str s)", "int");
+        add(treeItem, 0, "float(str s)", "float");
+        add(treeItem, 0, "double(str s)", "double");
+        add(treeItem, 0, "internal(str path)", "file");
+        add(treeItem, 0, "local(str path)", "file");
+        add(treeItem, 0, "external(str path)", "file");
+        add(treeItem, 0, "absolute(str path)", "file");
+        add(treeItem, 0, "classpath(str path)", "file");
         add(treeItem, 0, "rect(double x, double y, double width, double height)", "rect");
         add(treeItem, 0, "sprite(img x)", "sprite");
         add(treeItem, 0, "sprite(file f)", "sprite");
-        add(treeItem, 0, "sprite(string name)", "sprite");
+        add(treeItem, 0, "sprite(str name)", "sprite");
         add(treeItem, 0, "camera2d()", "camera2d");
         add(treeItem, 0, "point(double x, double y)", "point");
-        add(treeItem, 0, "color(string name)", "color");
-        add(treeItem, 0, "color(string name, float a)", "color");
+        add(treeItem, 0, "color(str name)", "color");
+        add(treeItem, 0, "color(str name, float a)", "color");
         add(treeItem, 0, "color(float r, float g, float b)", "color");
         add(treeItem, 0, "color(float r, float g, float b, float a)", "color");
-        add(treeItem, 0, "draw(string text)", "void");
-        add(treeItem, 0, "draw(string text, int x, int y)", "void");
-        add(treeItem, 0, "draw(font f, string text, int x, int y)", "void");
-        add(treeItem, 0, "draw(string text, int x, int y, color c)", "void");
-        add(treeItem, 0, "draw(font f, string text, int x, int y, color c)", "void");
+        add(treeItem, 0, "draw(str text)", "void");
+        add(treeItem, 0, "draw(str text, int x, int y)", "void");
+        add(treeItem, 0, "draw(font f, str text, int x, int y)", "void");
+        add(treeItem, 0, "draw(str text, int x, int y, color c)", "void");
+        add(treeItem, 0, "draw(font f, str text, int x, int y, color c)", "void");
         add(treeItem, 0, "draw(sprite s)", "void");
         add(treeItem, 0, "draw(sprite[] s)", "void");
         add(treeItem, 0, "fill(color c)", "void");
@@ -443,20 +444,20 @@ public class Documentation {
         add(treeItem, 0, "dst(float x1, float y1, float x2, float y2)", "float");
         add(treeItem, 0, "serialize(any value)", "byte[]");
         add(treeItem, 0, "deserialize(byte[] data)", "any value");
-        add(treeItem, 0, "keyDown(string name)", "bool");
-        add(treeItem, 0, "keyPressed(string name)", "bool");
-        add(treeItem, 0, "mouseDown(string name)", "bool");
-        add(treeItem, 0, "mouseClicked(string name)", "bool");
-        add(treeItem, 0, "mouseDouble(string name)", "bool");
-        add(treeItem, 0, "mouseReleased(string name)", "bool");
+        add(treeItem, 0, "keyDown(str name)", "bool");
+        add(treeItem, 0, "keyPressed(str name)", "bool");
+        add(treeItem, 0, "mouseDown(str name)", "bool");
+        add(treeItem, 0, "mouseClicked(str name)", "bool");
+        add(treeItem, 0, "mouseDouble(str name)", "bool");
+        add(treeItem, 0, "mouseReleased(str name)", "bool");
         add(treeItem, 0, "moveMouse(point p)", "void");
         add(treeItem, 0, "moveMouse(int x, int y)", "void");
-        add(treeItem, 0, "browse(string url)", "void");
-        add(treeItem, 0, "google(string term)", "void");
+        add(treeItem, 0, "browse(str url)", "void");
+        add(treeItem, 0, "google(str term)", "void");
         add(treeItem, 0, "noise(double x, double y)", "double");
         add(treeItem, 0, "noise(double x, double y, double z)", "double");
         add(treeItem, 0, "noise(double x, double y, double z, double w)", "double");
-        add(treeItem, 0, "path(point start, point goal, int width, int height, string isValidFuncName", "point[]");
+        add(treeItem, 0, "path(point start, point goal, int width, int height, func isValid", "point[]");
         add(treeItem, 0, "circle(float x, float y, float radius)", "void");
         add(treeItem, 0, "circle(float x, float y, float radius, int segments)", "void");
         add(treeItem, 0, "ellipse(float x, float y, float width, float height)", "void");
@@ -475,8 +476,8 @@ public class Documentation {
     private static void add(DocumentationTreeItem parent, int level, String value){
         if(level == 0) {
             parent.getChildren().add(new DocumentationTreeItem(value));
-            stringBuilder.append(" ");
-            stringBuilder.append(value.split("\\(", 2)[0]);
+            strBuilder.append(" ");
+            strBuilder.append(value.split("\\(", 2)[0]);
         }
         else
             add((DocumentationTreeItem)parent.getChildren().get(parent.getChildren().size() - 1), level - 1, value);
@@ -485,8 +486,8 @@ public class Documentation {
     private static void add(DocumentationTreeItem parent, int level, String value, String returnType){
         if(level == 0){
             parent.getChildren().add(new DocumentationTreeItem(value, returnType));
-            stringBuilder.append(" ");
-            stringBuilder.append(value.split("\\(", 2)[0]);
+            strBuilder.append(" ");
+            strBuilder.append(value.split("\\(", 2)[0]);
         }
         else
             add((DocumentationTreeItem)parent.getChildren().get(parent.getChildren().size() - 1), level - 1, value, returnType);
@@ -495,8 +496,8 @@ public class Documentation {
     private static void add(DocumentationTreeItem parent, int level, String value, boolean isReadOnly, String returnType){
         if(level == 0){
             parent.getChildren().add(new DocumentationTreeItem(value, returnType, isReadOnly));
-            stringBuilder.append(" ");
-            stringBuilder.append(value.split("\\(", 2)[0]);
+            strBuilder.append(" ");
+            strBuilder.append(value.split("\\(", 2)[0]);
         }
         else
             add((DocumentationTreeItem)parent.getChildren().get(parent.getChildren().size() - 1), level - 1, value, isReadOnly, returnType);
